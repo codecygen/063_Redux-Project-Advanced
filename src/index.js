dbb5617ecc74f40a252f8b2ab8a180c084955278
gingercook <vahitaras@gmail.com>
2022-03-21 20:34:03 -0400

    shopping cart visibility toggling is set up with redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store/index';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
